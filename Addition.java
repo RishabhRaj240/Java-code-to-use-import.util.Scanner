14249f38181ab232621639fbc09974f8801fe7fe
Rishabh Raj <rishabhraj240405@gmail.com>
2024-04-26 13:02:16 +0530

    Update Addition.java

--- Addition.java
+++ Addition.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 
 class Addition {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		Scanner scn = new Scanner(System.in);
+	Scanner scn = new Scanner(System.in);
 
-		System.out.print("Enter FNO: ");
-		int a = scn.nextInt();
+	System.out.print("Enter FNO: ");
+	int a = scn.nextInt();
 
-		System.out.print("Enter SNO: ");
-		int b = scn.nextInt();
+	System.out.print("Enter SNO: ");
+	int b = scn.nextInt();
 
-		int c = a + b;
-		System.out.println("Result: " + c);
-	}
+	int c = a + b;
+	System.out.println("Result: " + c);
+   }
 }
